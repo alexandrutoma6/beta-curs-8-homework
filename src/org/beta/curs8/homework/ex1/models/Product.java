8c453e253b8ef9dfe4bfec6d6c416e688cd0da49
Toma Alexandru <alextoma73.at@gmail.com>
2024-06-11 12:14:32 +0300

    Make fields final and remove setters

--- src/org/beta/curs8/homework/ex1/models/Product.java
+++ src/org/beta/curs8/homework/ex1/models/Product.java
@@ -2,10 +2,10 @@ package org.beta.curs8.homework.ex1.models;
 
 public class Product {
 
-    private Double price;
-    private String name;
-    private String description;
-    private Double quantity;
+    private final Double price;
+    private final String name;
+    private final String description;
+    private final Double quantity;
 
     public Product(Double price, String name, String description, Double quantity) {
         this.price = price;
@@ -18,31 +18,16 @@ public class Product {
         return price;
     }
 
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public Double getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(Double quantity) {
-        this.quantity = quantity;
-    }
 }
